35ddaf591803a679acf3efbe2608d89e5506b168
tanguytib <tanguytib@gmail.com>
2018-02-12 22:23:52 +0100

    Ajout du formulaire de recherche page d'accueil

--- html/accueil.php
+++ html/accueil.php
@@ -1,35 +1,25 @@
 <!DOCTYPE HTML>
 <html lang="fr">
-  <head>
-    <meta charset="utf-8">
-    <link rel="stylesheet" href="../css/styles.css">
-    <meta name="viewport" content="width=device-width">
-    <title>piano</title>
-  </head>
+	<head>
+		<meta charset="utf-8">
+		<link rel="stylesheet" href="../css/styles.css">
+		<meta name="viewport" content="width=device-width">
+		<title>piano</title>
+	</head>
     
-<body>
-    <header>
-        <div >
-            <p class ="mecenes">La Taverne</p>
-            <p class ="mecenes">Le Domaine</p>
-        </div>
-    <img src="../content/imgSite/piano.png" width ="200px">
+	<body>
+		<header>
+		</header>
 
-    </header>
-   
+		<!-- Formulaire de recherche -->
+		<form action="../html/search.php">
+		  <input type="text" name="fname" placeholder="Rechercher..." ><br>
+		  <input type="submit" value="" class="search_pic">
+		</form>
+		
+		<!-- Carrousel de records : affichage de quelques records avec flèches droites et gauches pour naviguer -->
 	
-	
-<?php
-	//Allez hop une petite DB histoire de bosser tranquille, on la changera bien sur quand le site sera en ligne
-$database = 'sql11220909';
-$username = 'sql11220909';
-$password = 'zD2yGIfB15';
-$hostname = 'sql11.freesqldatabase.com';
-$link = mysql_connect($hostname, $username, $password);
-$db_selected = mysql_select_db($database, $link);
-
-?>	
-	
-</body>
+		
+	</body>
 </html>
         
