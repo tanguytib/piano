23612fedb37709a5f24ea3b2188f0e2877a49727
gregoire120 <gregoiremarotte2@gmail.com>
2018-02-16 20:12:31 +0100

    insertion Nv record à DB

--- html/creation_record/insertion_record.php
+++ html/creation_record/insertion_record.php
@@ -1,8 +1,27 @@
 <?php
 	include '../DBconfig.php';
-
-	echo $categories = $db->query('SELECT * FROM Categories');
+	
 	
 
+	$requete = $db->prepare('INSERT INTO pnfra796140(nom, prenom, promo, numero_phare, status, categorie) VALUES( :intitule, :numero_phare, :status, :nom, :prenom, :promo, :categorie,)');
+	$requete->execute(array(
+		'intitule' => $_POST['intitule'],
+		'numero_phare' => $_POST['numero_phare'],
+		'status' => $_POST['status'],	
+		'nom' => $_POST['nom'],	
+		'prenom' => $_POST['prenom'],	
+		'promo' => $_POST['promo'],	
+		'categorie' => $_POST['categorie']
+		));
+	
+	echo  'Ton record à bien été ajouté pti pédé !';
+	header()
 
 ?>
+
+
+
+
+
+
+
